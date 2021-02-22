43197be3d72b40b08e8a089978902e0bc576aac7
Patrick Ribeiro <60348570+ptkribeiro@users.noreply.github.com>
2021-02-22 08:28:09 -0300

    Quarto Desafio

---
+++ Meta.Logica.Desafio4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Meta.Logica.Desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mapa = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+            int TotalRetido = 0;
+            for (int x = 0; x < mapa.Max() - 1; x++)
+            {
+                for (int y = 1; y < mapa.Length - 1; y++)
+                {
+                    int proximaBarra = mapa.ToList().FindIndex(y + 1, a => a > x);
+                    int aguaRetida = proximaBarra - y - 1;
+                    if (mapa[y] > x && aguaRetida > 0)
+                    {
+                        TotalRetido+= aguaRetida;
+                        y = proximaBarra - 1;
+                    }
+                }
+            }
+            Console.WriteLine(String.Format("A quantidade total de água retida é de {0}", TotalRetido));
+            Console.ReadKey();
+        }
+    }
+}
